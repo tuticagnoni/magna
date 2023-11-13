657161dcd6547a976542ac350e249388cc45a39e
tuticagnoni <tuti1997cagnoni@gmail.com>
2023-11-13 12:56:12 -0300

    comit

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,5 +1,5 @@
 <?php
- $to = "nadir.jafri90@gmail.com";
+ $to = "nadin@somosmagna.com";
  $subject = $_POST['subject'];
  $body = "From, ".$_POST['name']."\n\n"."Email, ".$_POST['email']."\n\n".$_POST['message'];
  if (mail($to, $subject, $body)) {
